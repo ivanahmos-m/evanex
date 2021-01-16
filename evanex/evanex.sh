52d559803d7c27118c0a64e1a5e7f233be830f07
ivanahmos-m <72409348+ivanahmos-m@users.noreply.github.com>
2021-01-16 23:45:51 +0200

    Add files via upload

---
+++ evanex/evanex.sh
@@ -0,0 +1,36 @@
+sudo apt-get install wireshark -y
+echo wireshark installation  finished
+sudo apt-get install aircrack-ng -y
+echo aircrack-ng installation finished
+sudo apt-get install john -y
+echo john the ripper installation finished
+sudo apt-get install hydra -y
+echo hydra installation finished
+sudo apt-get install python3 -y
+echo python 3 installation finished
+sudo apt-get install curl -y
+echo curl installation finished
+sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
+echo metasploit framework installation finished
+sudo git clone https://github.com/lanjelot/patator.git
+echo patator installation finished
+sudo git clone https://github.com/derv82/wifite.git
+echo wifite installation finished also almost finnished
+sudo apt-get install nmap -y
+
+echo nmap installation finnished
+
+sudo msfinstall
+
+echo you finnished congragulations
+
+
+
+
+
+
+
+
+echo also there is a background photo inside the folder plz use it also this project was made under 15 minutes so thanks
